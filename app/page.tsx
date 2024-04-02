cac82c2b2ffac042ead867488462636b7f57e43a
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-04-02 17:43:25 +0900

    :hammer: refector: 실시간 거래량, 인기 급상승 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,27 @@
 import Header from "@/app/ui/header";
-import Image from "next/image";
+import RankingItem from "@/app/ui/ranking-item";
+import Footer from "@/app/ui/footer";
 
 export default function Home() {
-  const liHTML = (
-    <li className="flex justify-between h-10 mt-5">
-      <div className="flex item-center items-center gap-4">
-        {/* <span className="mr-4">1</span> */}
-        <span>1</span>
-        <Image src="/samsung.png" width={40} height={40} alt="samsung" className="rounded-full" />
-        <div className="flex flex-col">
-          <span>삼성전자</span>
-          <span className="text-slate-500 text-sm font-light">005930</span>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <span className="font-semibold text-red-500 text-sm">85,000</span>
-        <span className="font-light text-red-500 text-xs">+3.66%</span>
-      </div>
-    </li>
-  );
+  const liHTML = new Array(10).fill(0).map((_, idx) => <RankingItem idx={idx} />);
 
   return (
     <>
       <Header />
       <main className="max-w-5xl w-full h-[65rem] mt-10">
         <div className="flex">
-          <section className="border w-1/2 p-4">
+          <section className="border w-1/2 p-4 rounded-lg mr-2">
             <h2 className="text-xl font-semibold pb-4">실시간 거래량</h2>
-            <ol>{new Array(10).fill(0).map((_) => liHTML)}</ol>
+            <ol>{liHTML}</ol>
           </section>
 
-          <section className="border w-1/2 p-4">
+          <section className="border w-1/2 p-4 rounded-lg">
             <h2 className="text-xl font-semibold pb-4">인기 급상승</h2>
-            <ol>{new Array(10).fill(0).map((_) => liHTML)}</ol>
+            <ol>{liHTML}</ol>
           </section>
         </div>
       </main>
-      <footer className="w-full h-[10rem] flex justify-center items-center bg-slate-50 text-slate-500 text-sm">
-        © 2024 stock mate
-      </footer>
+      <Footer />
     </>
   );
 }
--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Link className="flex flex-row items-center" href="/">
-      <Image src="/logo-image.png" width={40} height={100} alt="logo image" className="mr-2" />
-      <Image src="/logo-text.png" width={130} height={80} alt="logo text" />
+      <Image src="/logo-image.png" width={40} height={40} alt="logo image" className="mr-2" />
+      <Image src="/logo-text.png" width={125} height={40} alt="logo text" />
     </Link>
   );
 };
