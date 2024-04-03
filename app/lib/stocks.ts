0f230da99f001c1cc34647d49288ebc0aa380e0f
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-04-03 18:31:32 +0900

    :gift: feat: 검색 제안 키워드 ui 구현

--- app/lib/stocks.ts
+++ app/lib/stocks.ts
@@ -1,6 +1,6 @@
 const fetchData = async (url: string) => {
   try {
-    // console.log(url);
+    console.log(url);
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
@@ -12,8 +12,18 @@ const fetchData = async (url: string) => {
 };
 
 export const getStock = async (ticker: string) => {
+  console.log("call get stock");
   const url = `${process.env.API_URL}/stocks/${ticker}`;
   const data = await fetchData(url);
-  console.log(data);
+  // console.log(data);
+  return data;
+};
+
+export const getSuggestions = async (keyword: string) => {
+  console.log(process.env.API_URL);
+  console.log("call get suggestions");
+  const url = `${process.env.API_URL}/find_ticker/?comname=${keyword}`;
+  const data = await fetchData(url);
+  // console.log(data);
   return data;
 };
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,22 +1,57 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Logo from "@/app/ui/logo";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
+import { getSuggestions } from "@/app/lib/stocks";
+// 여기서 검색하면 믿에 뜨게금..
+// 검색어가 입력되면 디바운스 시작되야하고 데이터 fetch 해야함
 
 const Header = () => {
+  const [keyword, setKeyword] = useState("");
+  const [suggestion, setSuggestion] = useState<string[]>([]);
+
+  const handleKeyword = useDebouncedCallback((newKeyword) => {
+    console.log(newKeyword);
+    setKeyword(newKeyword);
+  }, 1000);
+
+  useEffect(() => {
+    // setSuggestion(["abc"]);
+    if (!keyword) return;
+
+    const fetchSuggestions = async (keyword: string) => {
+      const data = await getSuggestions(keyword);
+      console.log(data);
+    };
+
+    fetchSuggestions(keyword);
+  }, [keyword]);
+
   return (
     <header className="max-w-5xl w-full flex flex-row justify-between mt-4">
       <div className="basis-1/4">
         <Logo />
       </div>
-      <form className="flex flex-row basis-3/4 border rounded-md overflow-hidden">
-        <input
-          type="search"
-          placeholder="종목명 또는 코드를 입력해주세요"
-          className="w-full p-2 outline-none"
-        />
-        <button type="submit">
-          <MagnifyingGlassIcon className="text-slate-500 w-6 h-6 stroke-2 mr-2" />
-        </button>
-      </form>
+      <div className="flex flex-col	basis-3/4 border rounded-md overflow-hidden">
+        <form className="flex flex-row basis-3/4 border rounded-md overflow-hidden">
+          <input
+            type="search"
+            placeholder="영문 종목명을 입력하세요"
+            className="w-full p-2 outline-none"
+            onChange={(e) => handleKeyword(e.target.value)}
+          />
+          <button type="submit">
+            <MagnifyingGlassIcon className="text-slate-500 w-6 h-6 stroke-2 mr-2" />
+          </button>
+        </form>
+
+        <ul>
+          <li className="p-2 border">abc</li>
+          <li className="p-2 border">abc</li>
+        </ul>
+      </div>
     </header>
   );
 };
